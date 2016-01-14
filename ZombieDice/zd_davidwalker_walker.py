7d8e138896626052d7d969689d0676fa0a6a27f2
David Walker <walk.dv@gmail.com>
2016-01-13 21:54:32 -0400

    working combination finder

--- ZombieDice/zd_davidwalker_walker.py
+++ ZombieDice/zd_davidwalker_walker.py
@@ -12,52 +12,6 @@ from ZDengine import ZDPlayer
 
     Name both your file with the pattern: zd_yourname_ainame.py (file name), zd_yourname_ainame (Class name)
 '''
-def increment(indices, max_value):
-    '''
-        Increases the indices array to the next value, rolling over increases
-        in a given index to the previous index if it exceeds the highest allowed
-        index, resulting in a "counting" effect
-    '''
-    incrementing = len(indices)-1
-    while incrementing!=-1:
-        indices[incrementing]+=1
-        if indices[incrementing] == max_value:
-            indices[incrementing] = 0
-            incrementing -= 1
-        elif incrementing==-1:
-            return True
-        else:
-            return False
-def unique_increment(indices, max_value):
-    '''
-       Like increment, but will keep incrementing until no two indices
-       are identical. Note - if dice_required is greater than len(cup)
-       this will loop forever (or just generally not work)
-    '''
-    if increment(indices, max_value):
-        return True
-    while len(set(indices))<len(indices):
-        if increment(indices, max_value):
-            return True
-def hands(hand, cup):
-    '''
-        Returns a list of hands which might occur after drawing from the cup
-        Each hand is equally likely
-    '''
-
-    if len(hand)>=3:
-        return [hand]
-    diceRequired = 3 - len(hand)
-    indices = [n for n in range(diceRequired)]
-    possible_hands = []
-    finished = False
-    while not finished:
-        drawn_dice = []
-        drawn_dice += hand
-        for index in indices:
-            drawn_dice+=cup[index]
-        unique_increment(indices, len(cup))
-    return possible_hands
 class zd_davidwalker_walker(ZDPlayer):
     # must begin with zd_
     name = 'zd_walker'
@@ -78,21 +32,81 @@ class zd_davidwalker_walker(ZDPlayer):
 
     def Play(self, player_id, tallies, hand, cup, n_brain, n_shotgun):
         '''
-            The method called by the game to query a decision from the AI.
-            INPUT:
-               player_id (int) - the index in tallies of the AI player.
-               tallies (list of list of int) - List of scores from rounds for all players.
-               hand - (List of string) - List of dice in hand, coded by color (R,G,Y)
-               cup - (list of string) - Similar to hand, but for what is left in the cup
-               n_brain - (int) Current number of brains on the table
-               n_shotgun - (int) Current number of shotgun on the table.
-            OUTPUT:
-               True - (bool) Proceed with another round of dice rolling
-               False - (bool) Cash in the brains and end the turn
+        The method called by the game to query a decision from the AI.
+        INPUT:
+        player_id (int) - the index in tallies of the AI player.
+        tallies (list of list of int) - List of scores from rounds for all players.
+        hand - (List of string) - List of dice in hand, coded by color (R,G,Y)
+        cup - (list of string) - Similar to hand, but for what is left in the cup
+        n_brain - (int) Current number of brains on the table
+        n_shotgun - (int) Current number of shotgun on the table.
+        OUTPUT:
+        True - (bool) Proceed with another round of dice rolling
+        False - (bool) Cash in the brains and end the turn
 
-            STRATEGY:
-               ...
-         '''
+        STRATEGY:
+        ...
+        '''
         # Always cash in, regardless of game state.
-        print "I could have: "+str(hands(hand, cup))+"\n"
+        # print "I have "+ str(hand) + " in my hand"
+        # print "the cup is "+str(cup)
+        try:
+            print "I have "+str(n_brain)+" brains and "+str(n_shotgun)+" blasts"
+            print "I could have: "+str(len(hands(hand, cup)))+"different hands"
+            return True
+        except Exception as e:
+            print str(e)
+            input()
+            return False
+def hands(hand, cup):
+    '''
+    Returns a list of hands which might occur after drawing from the cup
+    Each hand is equally likely
+    '''
+
+    if len(hand)>=3:
+        return [hand]
+    diceRequired = 3 - len(hand)
+    if len(cup)<diceRequired:
+        diceRequired=len(cup)
+    indices = [n for n in range(diceRequired)]
+    possible_hands = []
+    finished = False
+    count = 0;
+    while not finished:
+        drawn_dice = []
+        for index in indices:
+            drawn_dice+=cup[index]
+        possible_hands.append(drawn_dice +hand)
+        finished = unique_increment(indices, len(cup))
+    return possible_hands
+def unique_increment(indices, max_value):
+    '''
+    Like increment, but will keep incrementing until no two indices
+    are identical. Note - if dice_required is greater than len(cup)
+    this will loop forever (or just generally not work)
+    '''
+    if increment(indices, max_value):
+        return True
+    while len(set(indices))<len(indices):
+        if increment(indices, max_value):
+            return True
+def increment(indices, max_value):
+    '''
+        Increases the indices array to the next value, rolling over increases
+        in a given index to the previous index if it exceeds the highest allowed
+        index, resulting in a "counting" effect
+    '''
+    incrementing = len(indices)-1
+    while incrementing!=-1:
+        indices[incrementing]+=1
+        if indices[incrementing] == max_value:
+            indices[incrementing] = 0
+            incrementing -= 1
+        else:
+            break
+    if incrementing==-1:
+        print "hey"
+        return True
+    else:
         return False
